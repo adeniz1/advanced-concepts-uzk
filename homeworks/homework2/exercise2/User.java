b66d1211954783d354b291fd5bc08f96a64a3c3e
ayden34 <adeniz1@smail.uni-koeln.de>
2025-04-24 11:14:52 +0200

    chore: moved individual homework packages into a parent homework package

---
+++ homeworks/homework2/exercise2/User.java
@@ -0,0 +1,13 @@
+package homework2.exercise2;
+
+class User {
+    String name = "Unknown";
+
+    public String getRole() {
+        return "General user";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
