24f63b0b8a9f0b3076503428e1a1099cc6398946
ayden34 <adeniz1@smail.uni-koeln.de>
2025-04-29 16:22:56 +0200

    feat: solve homework 3 exercise 2

--- homeworks/homework3/exercise2/Main.java
+++ homeworks/homework3/exercise2/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println(m1.getType());
         System.out.println(m2.getType());
         //m1.send(); // Uncomment this line and explain what happens
+        //m1.send() kann nicht aufgerufen werden, da der statische Typ "Message" ist, und Message keine Methode namens send hat.
+        // Wenn wir m1 in senen dynamischen Typ umwandeln, können wir m1.send() aufrufen.
 
 
     }
