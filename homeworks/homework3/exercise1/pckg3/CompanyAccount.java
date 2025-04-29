61537b2dd1a8da5de306d241e9ccaf2b2eb9ef97
ayden34 <adeniz1@smail.uni-koeln.de>
2025-04-29 15:32:07 +0200

    feat: solve exercise 1 of homework3

--- homeworks/homework3/exercise1/pckg3/CompanyAccount.java
+++ homeworks/homework3/exercise1/pckg3/CompanyAccount.java
@@ -4,5 +4,9 @@ import homework3.exercise1.pckg1.Account;
 
 public class CompanyAccount extends Account {
 
+    public CompanyAccount(String owner, double balance, int pin) {
+        super(owner, balance, pin);
+    }
+
     
 }
