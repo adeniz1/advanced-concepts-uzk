be40f7a7e61e735f5db02e791fc24e3d4a098c58
ayden34 <adeniz1@smail.uni-koeln.de>
2025-05-08 23:27:10 +0200

    feat: solve in class exercise 4

--- inClassExercises/sheet4/exercise2/FleetDemo.java
+++ inClassExercises/sheet4/exercise2/FleetDemo.java
@@ -3,12 +3,14 @@ package inClassExercises.sheet4.exercise2;
 public class FleetDemo {
     public static void main(String[] args) {
         // TODO: create a Truck and an EVan, test methods as described
-        Truck truck1 = new Truck("Truck1", 200, 500);
+        Truck truck1 = new Truck("Truck1", 1000, 1000);
         Evan evan1 = new Evan("Evan1",200, 600);
 
         System.out.println(truck1.getFuelLevel());
         truck1.canReach(300);
+        truck1.refuel(100);
         System.out.println(truck1.getFuelLevel());
+        //etwas funktioniert nicht
 
     }
 }
