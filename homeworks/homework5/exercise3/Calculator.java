a0bf9e4e47765f2c1a53eb33ce765409b2b075a6
ayden34 <adeniz1@smail.uni-koeln.de>
2025-05-14 16:44:08 +0200

    feat: solve homework 5

--- homeworks/homework5/exercise3/Calculator.java
+++ homeworks/homework5/exercise3/Calculator.java
@@ -1,5 +1,29 @@
 package homework5.exercise3;
 
 public class Calculator {
+    //Variante 1
+    public static double divide(double a, double b){
+        try {
+            return  (a / b );
+        } catch (ArithmeticException e) {
+            System.out.println("You can not divide by zero!");
+            System.out.println(e);
+            return Double.NaN ;
+        }
+
+    }
+ //...
+public static void main(String[] args){
+    // Variante 1
+    System.out.println(Calculator.divide(3, 0));
+
+    //Variante 2
+    try {
+        System.out.println(Calculator.divide(3, 0));
+    }
+    catch(ArithmeticException e) {
+        System.out.println("You can not divide by zero!");
+    }
+ }
     
 }
